fbb3919948bf51280c2f8fec526c57b69e9fba6e
aravindasiva <aravinda.siva@pego.co.uk>
2021-11-02 18:16:01 +0000

    added tesla cyber truck to canvas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { BakeShadows, OrbitControls, Stage } from '@react-three/drei'
+import Model from '../components/model'
 
 const Home = () => {
   return (
-    <div>clean up done !!</div>
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 0], fov: 40 }} style={{ position: 'absolute', left: 0, top: 0, bottom: 0 }}>
+      <Suspense fallback={null}>
+        <Stage environment="city" intensity={0.5}>
+          <Model position={[0, 0, 0]} />
+          {/* <Shoe scale={-1} rotation={[0, 0.5, Math.PI]} position={[0, 0, -2]} /> */}/
+        </Stage>
+        <BakeShadows />
+      </Suspense>
+      <OrbitControls autoRotate />
+    </Canvas>
   )
 }
 
