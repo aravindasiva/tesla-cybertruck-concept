950f23cad7396531d142e4d1c8de9f7bdd614e02
aravindasiva <aravinda.siva@pego.co.uk>
2021-11-03 15:55:03 +0000

    installed chakra ui and dependency to _app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { BakeShadows, OrbitControls, Stage } from '@react-three/drei'
+import { BakeShadows, OrbitControls, Stage, softShadows } from '@react-three/drei'
 import Model from '../components/model'
+import { Box } from '@chakra-ui/react'
 
 const Home = () => {
   return (
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 0], fov: 40 }} style={{ position: 'absolute', left: 0, top: 0, bottom: 0 }}>
+    <Box zIndex={1} w="full" h="100vh" bg="gray.background" p={[2, 4, 8]}>
+    <Canvas shadows dpr={[1, 2]} camera={{ fov: 45, zoom: 0.5 }} style={{ position: 'absolute', left: 0, top: 0, bottom: 0 }}>
       <Suspense fallback={null}>
-        <Stage environment="city" intensity={0.5}>
-          <Model position={[0, 0, 0]} />
-          {/* <Shoe scale={-1} rotation={[0, 0.5, Math.PI]} position={[0, 0, -2]} /> */}/
-        </Stage>
-        <BakeShadows />
+        <Model position={[0, -0.7, 0]} />
       </Suspense>
       <OrbitControls autoRotate={false} />
     </Canvas>
+    </Box>
   )
 }
 
