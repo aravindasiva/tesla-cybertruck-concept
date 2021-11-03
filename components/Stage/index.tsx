67943448a880f392ab47402acceeb76abaf8ed70
aravindasiva <aravinda.siva@pego.co.uk>
2021-11-03 17:56:54 +0000

    small changes

--- components/Stage/index.tsx
+++ components/Stage/index.tsx
@@ -7,22 +7,22 @@ export function Stage() {
         <ambientLight intensity={1.2} />
         {/* Main */}
         <directionalLight
-          position={[1, 10, -3]}
+          position={[1, 10, -5]}
           intensity={1}
           shadow-camera-far={70}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
+          shadow-camera-left={10}
+          shadow-camera-right={-10}
           shadow-camera-top={10}
           shadow-camera-bottom={-10}
           shadow-mapSize={[512, 512]}
           castShadow
         />
         {/* Strip */}
-        <directionalLight position={[-10, -10, 2]} intensity={3} />
+        <directionalLight position={[-10, -10, -1]} intensity={3} />
         {/* Ground */}
         <mesh receiveShadow rotation-x={-Math.PI / 2} position={[0, -0.75, 0]}>
           <planeGeometry args={[20, 20]} />
-          <shadowMaterial opacity={0.3} />
+          <shadowMaterial opacity={0.2} />
         </mesh>
         {/* This freezes the shadow map, which is fast, but the model has to be static  */}
         <BakeShadows />
--- components/Underlay/index.tsx
+++ components/Underlay/index.tsx
@@ -1,10 +1,12 @@
+import { Box, Center } from '@chakra-ui/layout'
 import React from 'react'
+import CyberTruckLogo from '../svg/CyberTruckLogo'
 
 const Underlay = () => {
   return (
-    <div>
-      Cool stuff Coming soon 
-    </div>
+    <Center>
+        <CyberTruckLogo color='#fff'/>
+    </Center>
   )
 }
 export default Underlay
